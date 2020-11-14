de8f946173d534a111d22d7f5da1a6da64cb7fd1
Lyashka <iuliana9864@gmail.com>
2020-11-14 20:05:51 +0300

    add introduction

--- components/layout.js
+++ components/layout.js
@@ -3,8 +3,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Juliana'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Juliana';
+export const siteTitle = 'Next.js Tutorial';
 
 export default function Layout({ children, home }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,9 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>I am Juliana. I am from Kazan, Russia.</p>
+        <p>I am doing frontend development with JS.</p>
+        <p>I'm learning Next.js right now, so I'm doing this tutorial :)</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
